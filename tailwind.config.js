39e4e0e19f8b520391eb5d2ccf4905612680e350
yilmaz <yilmazapis@gmail.com>
2023-01-22 17:37:14 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  // plugins: [require("@tailwindcss/forms")],
+};
